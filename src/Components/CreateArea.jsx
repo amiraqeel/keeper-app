986ef40bc3c698a7e9fb124a26bdc8f1d40f8e30
amiraqeel <amir.aqeel123@gmail.com>
2020-10-02 21:26:27 +0530

    intial commit

--- src/Components/CreateArea.jsx
+++ src/Components/CreateArea.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
 
@@ -6,6 +9,7 @@ function CreateArea(props) {
     title: "",
     content: ""
   });
+  const [active, setActive] = useState(false);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -14,6 +18,12 @@ function CreateArea(props) {
         ...prevState,
         [name]: value
       });
+    });
+  }
+
+  function handleFocus() {
+    setActive((prev) => {
+      return !prev;
     });
   }
 
@@ -28,11 +38,17 @@ function CreateArea(props) {
 
   return (
     <div>
-      <form onSubmit={submitNote}>
-        <input name="title" onChange={handleChange} placeholder="Title" value={note.title} />
-        <textarea name="content" onChange={handleChange} placeholder="Take a note..." rows="3" value={note.content} />
-        <button type="submit">Add</button>
+
+      <form onSubmit={submitNote} className="create-note">
+        {active && <input name="title" onChange={handleChange} placeholder="Title" value={note.title} autoComplete="off" />}
+        <textarea name="content" onChange={handleChange} placeholder="Take a note..." rows={active ? 3 : 1} value={note.content} onClick={handleFocus} />
+        {active && <Zoom in={active}>
+          <Fab type="submit">
+            <AddIcon />
+          </Fab>
+        </Zoom>}
       </form>
+
     </div>
   );
 }
